e619082c4324c92c22c162c226e002a768ec9a57
Anna Johnson <22annajohnson@gmail.com>
2022-11-08 13:50:38 -0500

    Added tab bar view controllers and icons. Added a SwiftUI View that will contain the summary screen items.

---
+++ BlackBox/MainTabBarViewController.swift
@@ -0,0 +1,39 @@
+//
+//  ViewController.swift
+//  BlackBox
+//
+//  Created by Anna Johnson on 11/8/22.
+//
+
+import UIKit
+
+class MainTabBarViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+        
+        let vc1 = UINavigationController(rootViewController: HomeViewController())
+        let vc2 = UINavigationController(rootViewController: VitalsViewController())
+        let vc3 = UINavigationController(rootViewController: EnvironmentViewController())
+        let vc4 = UINavigationController(rootViewController: TeamViewController())
+
+        vc1.tabBarItem.image = UIImage(systemName: "house")
+        vc2.tabBarItem.image = UIImage(systemName: "stethoscope")
+        vc3.tabBarItem.image = UIImage(systemName: "airplane")
+        vc4.tabBarItem.image = UIImage(systemName: "person.2.fill")
+        
+        
+        vc1.title = "Hanger"
+        vc2.title = "Vitals"
+        vc3.title = "Environment"
+        vc4.title = "Crew"
+        
+        
+        tabBar.tintColor = .systemBlue
+        
+        setViewControllers([vc1, vc2, vc3, vc4], animated: true)    }
+
+
+}
+
---
+++ BlackBox/SwiftUIViews/SummaryView.swift
@@ -0,0 +1,20 @@
+//
+//  SummaryView.swift
+//  BlackBox
+//
+//  Created by Anna Johnson on 11/8/22.
+//
+
+import SwiftUI
+
+struct SummaryView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SummaryView_Previews: PreviewProvider {
+    static var previews: some View {
+        SummaryView()
+    }
+}
